7dd1f2d61a99e9fb70f24bbbee6dec44c0023381
robertoRamos <rramcas2302@g.educaand.es>
2025-11-27 18:31:49 +0100

    ap1

---
+++ datos.php
@@ -0,0 +1,63 @@
+<?php
+$conceptos = [
+    [
+        'referencia' => 'MON-24LED',
+        'concepto' => 'Monitor LED 24" FullHD',
+        'unidades' => 2,
+        'precio_unidad' => 149.99
+    ],
+    [
+        'referencia' => 'TEC-MECA-RGB',
+        'concepto' => 'Teclado mecánico RGB',
+        'unidades' => 3,
+        'precio_unidad' => 79.95
+    ],
+    [
+        'referencia' => 'RAM-16GB-DDR4',
+        'concepto' => 'Memoria RAM 16GB DDR4',
+        'unidades' => 5,
+        'precio_unidad' => 64.50
+    ],
+    [
+        'referencia' => 'SSD-1TB-NVME',
+        'concepto' => 'Disco SSD 1TB NVMe',
+        'unidades' => 2,
+        'precio_unidad' => 129.99
+    ],
+    [
+        'referencia' => 'GPU-RTX3060',
+        'concepto' => 'Tarjeta gráfica RTX 3060 12GB',
+        'unidades' => 1,
+        'precio_unidad' => 399.00
+    ],
+    [
+        'referencia' => 'MOUSE-GAMING',
+        'concepto' => 'Ratón gaming óptico 16000 DPI',
+        'unidades' => 4,
+        'precio_unidad' => 49.95
+    ],
+    [
+        'referencia' => 'CPU-R5-5600X',
+        'concepto' => 'Procesador AMD Ryzen 5 5600X',
+        'unidades' => 1,
+        'precio_unidad' => 269.99
+    ],
+    [
+        'referencia' => 'CASE-ATX-RGB',
+        'concepto' => 'Caja ATX con iluminación RGB',
+        'unidades' => 2,
+        'precio_unidad' => 89.95
+    ],
+    [
+        'referencia' => 'PSU-750W-80PLUS',
+        'concepto' => 'Fuente de alimentación 750W 80 Plus Gold',
+        'unidades' => 2,
+        'precio_unidad' => 109.50
+    ],
+    [
+        'referencia' => 'WIFI-ADAPTER',
+        'concepto' => 'Adaptador WiFi USB dual band',
+        'unidades' => 5,
+        'precio_unidad' => 24.99
+    ]
+];
